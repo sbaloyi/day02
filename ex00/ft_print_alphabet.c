a0b40c4b35fee965ed31bd711bfae38358f8e3c7
Sam Baloyi <sbaloyi@c4r8s5.wethinkcode.co.za>
2020-06-18 08:23:40 +0200

    Updated ex00

--- ex00/ft_print_alphabet.c
+++ ex00/ft_print_alphabet.c
@@ -1,18 +1,13 @@
-#include <unistd.h>
-
-void ft_putchar(char c) { write(STDOUT_FILENO, &c, 1); }
-
 void ft_print_alphabet(void) {
-	  char letter;
-
-	    letter = 'a';
-	      while (letter <= 'z') {
-		          ft_putchar(letter);
-			      letter++;
-			        }
+    char letter = 'a';
+    while (letter <= 'z') {
+        putchar(letter);
+        letter++;
+    }
 }
 
-int main(void) {
-	  ft_print_alphabet();
-	    return 0;
+int main(void)
+{
+    ft_print_alphabet();
+    return 0;
 }
