54c04f536f1ef56dad5f9e7333c6692e61bd14a0
sbaloyi <66995239+sbaloyi@users.noreply.github.com>
2020-07-22 22:20:19 +0200

    Update ft_is_negative.c

--- ex03/ft_is_negative.c
+++ ex03/ft_is_negative.c
@@ -1,7 +1,8 @@
 #include <unistd.h>
 
-void ft_is_negative(int);
-void ft_putchar(char n);
+void ft_putchar(char a) {
+	  write(1, &a, 1);
+}
 
 void ft_is_negative(int n) { 
 	if (n >= 0) {
@@ -9,8 +10,4 @@ void ft_is_negative(int n) {
 	} else if (n < 0) {
 		ft_putchar('N');
 	 }    
-}
-
-void ft_putchar(char a) {
-	  write(1, &a, 1);
 }
